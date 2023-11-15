715376ef065ddffe7623d4ccf2a94209df66a297
BobZhang26 <141781876+BobZhang26@users.noreply.github.com>
2023-11-14 22:02:25 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,38 @@
-"""
-Main cli or app entry point
-"""
-def add(x,y):
-    return x + y
+use std::env;
+use rusqlite::{Connection, Result};
 
-if __name__ == "__main__":
-    add(1,1)
+
+fn main() {
+    if let Err(err) = run() {
+        eprintln!("Error: {:?}", err);
+    }
+}
+
+fn run() -> Result<(), rusqlite::Error> {
+    let conn = Connection::open("Transactions.db")?;  
+
+    println!("Creating table...");
+    DBFunc::create(&conn)?;
+        
+    println!("Inserting data...");
+    DBFunc::insert(&conn)?;
+    
+    println!("Querying data...");
+    DBFunc::query(&conn)?;
+
+    println!("Updating data...");
+    DBFunc::update(&conn)?;
+
+    println!("Querying data...");
+    DBFunc::query(&conn)?;
+
+    println!("Deleting data...");
+    DBFunc::delete(&conn)?;
+
+    println!("Querying data...");
+    DBFunc::query(&conn)?;    
+    
+    DBFunc::drop(&conn)?;    
+
+    Ok(())
+}
